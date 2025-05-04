a9d253422de728c0fa322c1e494b8445552c5dba
Artifice-219 <johnphilliplormalbas@gmail.com>
2025-05-04 08:09:09 -0700

    task can now be viewed individually using an index

--- todoList/Program.cs
+++ todoList/Program.cs
@@ -5,6 +5,8 @@ using System.Net.Mime;
 
 namespace Myapp{
     class TodoItem{
+        // this index will be used to identify the current task for modifications
+        public int index {get; set;}
         public string Content { get; set; }
         public string Time_created {get; set;}
 
@@ -15,25 +17,41 @@ namespace Myapp{
 
         // internal method to show details of this todo item
         public string showDetails(){
-            return $"Task : {Content} --- Time Created {Time_created}";
+            return $"Task {index} : {Content} --- Time Created {Time_created}";
         }
     }
     
     internal class Program{
+        static void viewTask(List<TodoItem> taskList){
+            // shows all of the task and then ask for which task to display
+            listTask(taskList);
+            Console.WriteLine();
+            Console.WriteLine("Select which task index to view. If no index is provided will default to zero");
+            string input = Console.ReadLine() ?? "0";
+            // cast input as int
+            int userChoice;
+            if(int.TryParse(input, out userChoice)){
+                // succesfull parsing
+                Console.WriteLine($"Index : {userChoice} selected");
+            }
+            else{
+                // if the parsing was unsuccessfull just go back to main menu ?
+                // this might prove idiotic in the long run
+                Console.WriteLine("Invalid input.");
+            }
+            Console.WriteLine($"Showing details for task indexed at : {userChoice}");
+            Console.WriteLine($"Task Details : {taskList[userChoice].Content}");
+            Console.WriteLine($"Created : {taskList[userChoice].Time_created}");
+             
+        }
         static void listTask(List<TodoItem> taskList){
             Console.WriteLine("Showing all task");
             // loops to every item in the taskList and display all its contents
             for(int i=0; i <= taskList.Count - 1; ++i){
-                // string taskDetails = taskList[i].Content;
-                // string taskTimeCreated = taskList[i].Time_created;
-                // Console.WriteLine($"{i} Task : {taskDetails} created at time : {taskTimeCreated}");
+                // set the index
+                taskList[i].index = i;
                 Console.WriteLine(taskList[i].showDetails()); 
             }
-            // foreach(TodoItem item in taskList){
-            //     string taskDetails = item.Content;
-            //     string taskTimeCreated = item.Time_created;
-            //     Console.WriteLine($"Task : {taskDetails} created at time : {taskTimeCreated}");
-            // }
         }
         // creates a new task and then append that to the task container
         static void addTask(List<TodoItem> taskList){
@@ -71,12 +89,19 @@ namespace Myapp{
         // main method
         static void Main(string[] args){
             List<TodoItem> TaskList = new List<TodoItem>();
+            // some builtin values, delete afterwards
+            TodoItem todoItem1 = new TodoItem("get laundry", "12-12-12");
+            TodoItem todoItem2 = new TodoItem("feed the fish", "12-13-12");
+            TodoItem todoItem3 = new TodoItem("Watch Movie", "12-14-12");
+            TodoItem todoItem4 = new TodoItem("Water the Plants", "12-15-12");
+            TaskList.Add(todoItem1);
+            TaskList.Add(todoItem2);
+            TaskList.Add(todoItem3);
+            TaskList.Add(todoItem4);
             // load main menu
             Menu();
-            addTask(TaskList);
-            addTask(TaskList);
             listTask(TaskList);
-            Console.WriteLine($"Current count of the tasklist contianer is {TaskList.Count}");
+            viewTask(TaskList);
         }
     }
 }
